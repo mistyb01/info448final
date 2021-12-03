4f2a178069f74de3e2813517526873a699297880
Misty Becker <mistyb01@uw.edu>
2021-12-03 14:28:22 -0800

    created an empty activity project

---
+++ app/src/main/java/edu/uw/mistyb01/recipefinder/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.uw.mistyb01.recipefinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
